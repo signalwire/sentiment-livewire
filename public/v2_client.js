b012b075ec6133f95d4eaf444076e7a7e894d587
Luca Pradovera <luca.pradovera@gmail.com>
2021-08-24 14:43:38 +0200

    REport WebRTC stats

--- public/v2_client.js
+++ public/v2_client.js
@@ -1,7 +1,11 @@
 var client;
 var currentCall = null;
+let _statsInterval = null;
+
+var _timer = null
 
 ready(function() {
+  _timer = performance.now();
   connect();
 });
 
@@ -20,7 +24,7 @@ function connect() {
       }
     ]
 
-  client.__logger.setLevel(client.__logger.levels.DEBUG)
+  client.__logger.setLevel(client.__logger.levels.INFO)
 
   client.remoteElement = 'remoteVideo';
   client.localElement = 'localVideo';
@@ -31,6 +35,8 @@ function connect() {
   client.on('signalwire.ready', function() {
     setStatus('Registered to SignalWire');
     show('callForm');
+    reportTimerStat('client connect', performance.now() - _timer);
+    _timer = null
   });
 
   // Update UI on socket close
@@ -99,13 +105,15 @@ function handleCallUpdate(call) {
       setStatus('Call is active');
       hide('callbtn');
       show('hangupbtn');
-      currentCall._stats();
+      toggleStats();
+      reportTimerStat('call setup', performance.now() - _timer);
+      _timer = null
       break;
     case 'hangup': // Call is over
       setStatus('Ready');
       show('callbtn');
       hide('hangupbtn');
-      currentCall._stats(false);
+      toggleStats();
       break;
     case 'destroy': // Call has been destroyed
       currentCall = null;
@@ -117,6 +125,7 @@ function handleCallUpdate(call) {
   * Make a new outbound call
 */
 function makeCall() {
+  _timer = performance.now();
   var destination = document.getElementById('destination').value + '@' + app;
   console.log('Calling ', destination);
   const params = {
@@ -151,6 +160,68 @@ function hide(selector) {
 
 function setStatus(text) {
   document.getElementById("status").innerHTML = text;
+}
+
+async function toggleStats() {
+  if (_statsInterval) {
+    return clearInterval(_statsInterval)
+  }
+  // Start the loop every 2 secs
+  _statsInterval = window.setInterval(async () => {
+    const stats = await currentCall.peer.instance.getStats(null)
+    const keys = ['jitter', 'packetsLost', 'roundTripTime', 'roundTripTimeMeasurements', 'timestamp', 'totalRoundTripTime' ];
+    stats.forEach(report => {
+      if (report.type == 'remote-inbound-rtp') {
+        /*
+        codecId: "RTCCodec_audio_Outbound_111"
+        fractionLost: 0
+        id: "RTCRemoteInboundRtpAudioStream_1112257779"
+        jitter: 0.006333333333333333
+        kind: "audio"
+        localId: "RTCOutboundRTPAudioStream_1112257779"
+        packetsLost: 1
+        roundTripTime: 0.162
+        roundTripTimeMeasurements: 2
+        ssrc: 1112257779
+        timestamp: 1629302066960.565
+        totalRoundTripTime: 0.308
+        transportId: "RTCTransport_audio_1"
+        type: "remote-inbound-rtp"
+        */
+        console.log('report', report);
+
+        var elm = document.createElement('div');
+        var titleElm = document.createElement('h5');
+        titleElm.innerText = 'WebRTC Report';
+        elm.appendChild(titleElm);
+
+        keys.forEach(k => {
+          console.log(k)
+          var line = document.createElement('div');
+          line.innerHTML = "<b>" + k + "</b>: " + report[k];
+          console.log(line)
+          elm.appendChild(line);
+        })
+
+        document.getElementById('stats').appendChild(elm);
+      }
+    })
+    
+  }, 2000)
+}
+
+function reportTimerStat(title, stat) {
+  console.log(title, stat)
+  var elm = document.createElement('div');
+  var titleElm = document.createElement('h5');
+  titleElm.innerText = title
+  elm.appendChild(titleElm);
+
+  var bodyElm = document.createElement('div');
+  bodyElm.innerText = Math.round(stat) + ' ms';
+  elm.appendChild(bodyElm);
+
+  document.getElementById('stats').appendChild(elm);
 }
 
 // jQuery document.ready equivalent
